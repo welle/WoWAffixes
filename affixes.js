41f971e836b8f301580262909937836fa2d83753
Welle Charlotte <charlottewelle@yahoo.fr>
2018-08-30 14:39:49 +0200

    new layout

--- affixes.js
+++ affixes.js
@@ -1,22 +1,42 @@
-function Affix(affixDate, affixValues, affixWeekTitle) {
+function Affixes(affixDate, affixValues, affixWeekTitle) {
     this.affixDate = affixDate;
     this.affixValues = affixValues;
     this.affixWeekTitle = affixWeekTitle;
 }
 
-var affixesTurn = [
-    [6, 4, 9],
-    [7, 2, 10],
-    [5, 3, 9],
-    [8, 12, 10],
-    [7, 13, 9],
-    [11, 14, 10],
-    [6, 3, 9],
-    [5, 13, 10],
-    [7, 12, 9],
-    [8, 4, 10],
-    [11, 2, 9],
-    [5, 14, 10],
+function Affix(level2, level4, level7, level10) {
+    this.level2 = level2;
+    this.level4 = level4;
+    this.level7 = level7;
+    this.level10 = level10;
+}
+
+var affixesTurnLevel2 = [
+    9, // Tyrannical
+    10 // Fortified
+];
+
+var affixesTurnLevel4 = [
+    7, // Bolstering
+    6, // Raging
+    8, // Sanguine
+    5, // Teeming
+    11 // Bursting
+];
+
+var affixesTurnLevel7 = [
+    //1, // Overflowing
+    4, // Necrotic
+    2, // Skittish
+    3, // Volcanic
+    13, // Explosive
+    14, // Quaking
+    12 // Grievous
+];
+
+var affixesTurnLevel10 = [
+    15, // Relentless
+    16 // Infested
 ];
 
 var affixWeekTitleTurn = [
@@ -33,7 +53,7 @@ function getWeekNumber(d) {
     d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
     // Set to nearest Thursday: current date + 4 - current day number
     // Make Sunday's day number 7
-    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
+    //d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
     // Get first day of year
     var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
     // Calculate full weeks to nearest Thursday
@@ -41,15 +61,49 @@ function getWeekNumber(d) {
     return weekNo;
 }
 
+function getClosestWednesday() {
+    var date = new Date();
+    date.setDate(date.getDate());
+    var day = date.getDay();
+    var closestWednesday;
+    if (day == 3) {
+        closestWednesday = new Date().setDate(date.getDate());
+    }
+    else {
+        if (day > 3) {
+            closestWednesday = new Date().setDate(date.getDate() - (day - 3));
+        } else {
+            closestWednesday = new Date().setDate(date.getDate() - day - 4);
+        }
+    }
+
+    return new Date(closestWednesday);
+}
+
+function getAffixByWeek(weekNumber) {
+    var roundLevel2 = weekNumber % (affixesTurnLevel2.length);
+    var roundLevel4 = weekNumber % (affixesTurnLevel4.length);
+    var roundLevel7 = weekNumber % (affixesTurnLevel7.length);
+    var roundLevel10 = weekNumber % (affixesTurnLevel10.length);
+
+    var affixLevel2 = affixesTurnLevel2[roundLevel2];
+    var affixLevel4 = affixesTurnLevel4[roundLevel4];
+    var affixLevel7 = affixesTurnLevel7[roundLevel7];
+    var affixLevel10 = affixesTurnLevel10[roundLevel10];
+
+    return new Affix(affixLevel2, affixLevel4, affixLevel7, affixLevel10);
+}
+
 exports.getAffixes = function () {
-    var currentDate = new Date();
-    var result = getWeekNumber(currentDate);
+    var closestWednesday = getClosestWednesday();
+    var result = getWeekNumber(closestWednesday);
 
     var functionResult = [];
     for (i = 0; i < 6; i++) {
-        var affixDate = new Date(currentDate);
+        var affixDate = new Date(closestWednesday);
         affixDate.setDate(affixDate.getDate() + (i * 7));
-        var affix = new Affix(affixDate, affixesTurn[(result + i) % (affixesTurn.length)], affixWeekTitleTurn[i]);
+        var currentAffixes = getAffixByWeek((result + (i * 7)));
+        var affix = new Affixes(affixDate, currentAffixes, affixWeekTitleTurn[i]);
         functionResult.push(affix);
     }
 
